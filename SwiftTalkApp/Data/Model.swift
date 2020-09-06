e9698b86b58d84a502709fea9acf82f1ad8c4685
Abdullah Althobetey <abdullah.th@me.com>
2020-09-06 20:25:55 +0300

    fix date decoding

--- SwiftTalkApp/Data/Model.swift
+++ SwiftTalkApp/Data/Model.swift
@@ -29,24 +29,28 @@ extension EpisodeView {
     var durationAndDate: String {
         "\(TimeInterval(media_duration).hoursAndMinutes) - \(dateFormatter.string(from: released_at))"
     }
+    
+    var mediaURL: URL? {
+        self.hls_url ?? self.preview_url
+    }
 }
 
 let allCollections = Endpoint<[CollectionView]>(
     json: .get,
-    url: URL(string: "https://talk.objc.io/collections.json")!
+    url: URL(string: "https://talk.objc.io/collections.json")!,
+    decoder: decoder
 )
 
 let allEpisodes = Endpoint<[EpisodeView]>(
-    json: .get,
-    url: URL(string: "https://talk.objc.io/episodes.json")!
+    json: .get
+    , url: URL(string: "https://talk.objc.io/episodes.json")!,
+      decoder: decoder
 )
 
 
 func sample<A: Codable>(name: String) -> A {
     let url = Bundle.main.url(forResource: name, withExtension: "json")!
     let data = try! Data(contentsOf: url)
-    let decoder = JSONDecoder()
-    decoder.dateDecodingStrategy = .secondsSince1970
     return try! decoder.decode(A.self, from: data)
 }
 
